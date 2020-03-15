b6c7e51708bb2ad9dbd20f024aa37faf6fe286fe
yulbka <yulbka13@mail.ru>
2020-03-15 12:33:09 +0300

    feat: add slider carousel

--- script.js
+++ script.js
@@ -1,5 +1,54 @@
+// active link anchor navigation
+
 let navigation = document.getElementById('navigation');
 navigation.addEventListener('click', function(event) {
   navigation.querySelectorAll('a').forEach(elem => elem.classList.remove('active'));
   event.target.closest('a').classList.add('active');
+})
+
+// slider carousel
+
+let carouselItems = document.querySelectorAll('.iphone-container');
+let currentItem = 0;
+let isEnabled = true;
+
+
+function hideItem(direction) {
+	isEnabled = false;
+	carouselItems[currentItem].classList.add(direction);
+	carouselItems[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('iphone-container_active', direction);
+	});
+}
+
+function showItem(direction) {   
+  carouselItems[currentItem].classList.add('next', direction);  
+  document.querySelector('.slider').classList.toggle('slider_carousel'); 
+  carouselItems[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('next', direction);
+    this.classList.add('iphone-container_active');
+    isEnabled = true;
+  })
+}
+
+document.querySelector('.arrow_prev').addEventListener('click', function() {
+  if (isEnabled) {
+    hideItem('to-right');    
+    currentItem -= 1; 
+    if (currentItem < 0) {
+      currentItem = carouselItems.length - 1;
+    }   
+    showItem('from-left');
+  }
+})
+
+document.querySelector('.arrow_next').addEventListener('click', function() {
+  if (isEnabled) {
+    hideItem('to-left');    
+    currentItem += 1;    
+    if (currentItem > carouselItems.length - 1) {
+      currentItem = 0;
+    }
+    showItem('from-right');
+  }
 })
